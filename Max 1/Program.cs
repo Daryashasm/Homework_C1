77335056866e9d3790084c530c7e455a129a69f6
Daria <darena-smol@yandex.ru>
2022-05-31 00:46:57 +0500

    11

---
+++ Max 1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число 1; А = ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2; B = ");
+int numberB = int.Parse(Console.ReadLine());
+Console.Write("Введите число 3; C = ");
+int numberC = int.Parse(Console.ReadLine());
+if (numberA > numberB && numberA > numberC)
+{
+    Console.WriteLine($"Максимальное число - A = {numberA}");
+}
+else if (numberB > numberC)
+{
+     Console.WriteLine($"Максимальное число - B = {numberB}");
+}
+else
+{
+     Console.WriteLine($"Максимальное число - C = {numberC}");
+}
