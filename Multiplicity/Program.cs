e86a8e0e385928ccb4467e916e6efe6f05b65517
Daria <darena-smol@yandex.ru>
2022-05-31 00:47:30 +0500

    4

---
+++ Multiplicity/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число; А = ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число; B = ");
+int numberB = int.Parse(Console.ReadLine());
+if (numberA % numberB == 0)
+{
+    Console.WriteLine($"{numberA} кратно числу {numberB}");
+}
+else
+{
+    int n = ((numberA * 10) / numberB) % 10;
+     Console.WriteLine($"{numberA} не кратно числу {numberB}, остаток после деления данных чисел друг на друга составляет - {n}");
+}
