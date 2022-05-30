f7598a44b11bb683664d5845b014b57ff02293b9
Daria <darena-smol@yandex.ru>
2022-05-31 00:43:55 +0500

    22

---
+++ Even number/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число; А = ");
+int numberA = int.Parse(Console.ReadLine());
+if (numberA % 2 == 0)
+{
+    Console.WriteLine($"Число A = {numberA} - четное");
+}
+else
+{
+     Console.WriteLine($"Число - А = {numberA} - нечетное");
+}
