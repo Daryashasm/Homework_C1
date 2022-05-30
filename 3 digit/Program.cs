6cdc46c70e50d367caf589daf5aac7368de3b420
Daria <darena-smol@yandex.ru>
2022-05-31 00:46:20 +0500

    1

---
+++ 3 digit/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите трехзначное число; А = ");
+int numberA = int.Parse(Console.ReadLine());
+if (numberA > 99 && numberA < 1000)
+{
+    int number = (numberA % 10);
+    Console.WriteLine($"{number} - это последняя цифра введенного трехзначного числа");
+}
+else
+{
+     Console.WriteLine($"{numberA} - это не трехзначное число");
+}
